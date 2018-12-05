065aa54c5839de19ec71425e6e4e493b50b8627f
OhnkytaBlabdey <1263189143@qq.com>
2018-12-05 22:13:12 +0800

    songs list

--- FunnyMusic/src/gui/InfoItem.java
+++ FunnyMusic/src/gui/InfoItem.java
@@ -1,43 +1,61 @@
 package gui;
 
-import java.awt.BorderLayout;
+//import java.awt.BorderLayout;
+//import java.awt.GridLayout;
+
 
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
+
+//import javax.swing.Icon;
+//import javax.swing.JFrame;
+//import javax.swing.JTextArea;
 
 public class InfoItem extends JLabel {
 
-	public static void main(String[] args) {
-		JFrame frame=new JFrame();
-		GlobalVars.frame=frame;
-		MainFrame.init();
-		frame.setLayout(new BorderLayout());
-		frame.setIconImage(new ImageIcon("conf/textures/logo.png").getImage());
-		
-		frame.add(new PlayBar(),BorderLayout.PAGE_END);
-		frame.add(new TitleBar(),BorderLayout.PAGE_START);
-		frame.add(new SceneBG(),BorderLayout.CENTER);
-		frame.add(new InfoItem(), BorderLayout.LINE_START);
-		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocation(300, 130);
-		frame.setSize(300, 300);
-		frame.setUndecorated(true);
-		frame.setVisible(true);
-	}
+//	public static void main(String[] args) {
+//		JFrame frame=new JFrame();
+//		GlobalVars.frame=frame;
+//		MainFrame.init();
+//		frame.setLayout(new BorderLayout());
+//		frame.setIconImage(new ImageIcon("conf/textures/logo.png").getImage());
+//		
+//		frame.add(new PlayBar(),BorderLayout.PAGE_END);
+//		frame.add(new TitleBar(),BorderLayout.PAGE_START);
+//		frame.add(new SceneBG(),BorderLayout.CENTER);
+//		frame.add(new InfoItem(), BorderLayout.LINE_START);
+//		
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		frame.setLocation(300, 130);
+//		frame.setSize(300, 300);
+//		frame.setUndecorated(true);
+//		frame.setVisible(true);
+//	}
+	
+	ImageIcon album;
+//	JLabel text1;
+	String song;
 	
 	public InfoItem() {
 		// TODO Auto-generated constructor stub
+		
+//		setLayout(new GridLayout(1, 2));
+		
 		setAlbum("album/album1.jpg");
-		setSongName("name - name");
+		setSongName("name - song1");
+		
+		
 	}
 	
 	public void setAlbum(String path) {
 		setIconTextGap(4);
-		setIcon(new ImageIcon(path));
+//		setIcon(new ImageIcon(path));
+		album=new ImageIcon(path);
+		setIcon(album);
+//		add(album);
 	}
 	public void setSongName(String s) {
 		setText(s);
+//		text1.setText(s);
 	}
 }
---
+++ FunnyMusic/src/gui/SongList.java
@@ -0,0 +1,54 @@
+package gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class SongList extends JPanel {
+	public static void main(String[] args) {
+		JFrame frame=new JFrame();
+		GlobalVars.frame=frame;
+		MainFrame.init();
+		frame.setLayout(new BorderLayout());
+		frame.setIconImage(new ImageIcon("conf/textures/logo.png").getImage());
+		
+		frame.add(new PlayBar(),BorderLayout.PAGE_END);
+		frame.add(new TitleBar(),BorderLayout.PAGE_START);
+		frame.add(new SceneBG(),BorderLayout.CENTER);
+		frame.add(new SongList(),BorderLayout.LINE_START);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocation(300, 130);
+		frame.setSize(300, 300);
+		frame.setUndecorated(true);
+		frame.setVisible(true);
+	}
+	DefaultListModel<InfoItem> model;
+	JList<InfoItem> list;
+	JScrollPane scrollPane;
+	public SongList() {
+		// TODO Auto-generated constructor stub
+		model=new DefaultListModel<InfoItem>();
+		list=new JList<InfoItem>(model);
+		scrollPane=new JScrollPane(list);
+		
+		add(scrollPane);
+		
+		addSong(new InfoItem());
+		
+	}
+	
+	public void addSong(InfoItem item) {
+		model.addElement(item);
+	}
+	
+	public void removeSong(InfoItem item) {
+	//TODO		
+	}
+	
+}
