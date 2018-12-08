7e5f60eedb97e180028f0459beda6337ef42c4cc
OhnkytaBlabdey <1263189143@qq.com>
2018-12-08 15:44:15 +0800

    release

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 @echo off
 java -jar funny_music.jar 1>>log.txt
-rem ## to avoid console run this :
-rem [start java -jar funny_music.jar 1>>log.txt]
-pause
+rem ## see console, run this :
+rem start java -jar funny_music.jar 1>>log.txt
+
+rem pause
